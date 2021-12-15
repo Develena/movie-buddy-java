4866fbb2c5060e7d5436fcc2c04447f53a036604
hyunhee <hyunhee@nuritelecom.com>
2021-12-15 19:47:49 +0900

    [section_04_03] 영화검색 컴포넌트 테스트를  JUnit 기반으로 변경하기

--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -1,6 +1,8 @@
 package moviebuddy.domain;
 
 import moviebuddy.MovieBuddyFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -9,25 +11,22 @@ import java.util.List;
  */
 public class MovieFinderTest {
 
-	public static void main(String[] args) {
+	final MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();
 
-		MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();
+	final MovieFinder movieFinder = movieBuddyFactory.movieFinder();
 
-		MovieFinder movieFinder = movieBuddyFactory.movieFinder();
-
+	@Test
+	public void NotEmpty_directedBy(){
 		// 마이클 베이 감독의 작품은 3편인 것을 테스트함.
-		List<Movie> result = movieFinder.directedBy("Michael Bay");
-		assertEquals(3, result.size());
+		List<Movie> movies = movieFinder.directedBy("Michael Bay");
+		Assertions.assertEquals(3, movies.size());
+	}
 
+	@Test
+	public void NotEmpty_releasedYearBy(){
 		// 2015년도에 출시된 영화는 225편인 것을 테스트함.
-		result = movieFinder.releasedYearBy(2015);
-    	assertEquals(225, result.size());
+		List<Movie> movies = movieFinder.releasedYearBy(2015);
+		Assertions.assertEquals(225, movies.size());
 	}
-	
-	static void assertEquals(long expected, long actual) {
-		if (expected != actual) {
-			throw new RuntimeException(String.format("actual(%d) is different from the expected(%d)", actual, expected));			
-		}
-	}
-	
+
 }
